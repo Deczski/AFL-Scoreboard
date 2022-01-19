e819241c60794772a910f6bd4ce2ef334e56deb2
Deczski <deczski@gmail.com>
2022-01-19 10:57:05 +0100

    front ready, Js started

---
+++ script.js
@@ -0,0 +1,10 @@
+let homeTeamGoals = 0;
+let homeTeamBehinds = 0;
+let homeTeamTotal = 0;
+
+let awayTeamGoals = 0;
+let awayTeamBehinds = 0;
+let awayTeamTotal = 0;
+
+let matchTime = 0;
+let whichQuarter = 1;
